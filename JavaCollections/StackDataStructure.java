6110027e6f551d9dd7043cf266e664794b2ef48e
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-02-28 05:41:57 +0530

    Learning Stack

---
+++ JavaCollections/StackDataStructure.java
@@ -0,0 +1,25 @@
+package JavaCollections;
+import java.util.*;
+import java.util.Stack;
+
+public class StackDataStructure {
+
+    public static void main(String[] args) {
+        //in this it maintedn the ibsertion order
+    // so not as much good comparing to c
+    //7 ayaa to print method se pahale 7 print hoga 
+    Stack<Integer> stack = new Stack<>();
+    stack.push(8);
+    stack.push(9);
+    stack.push(17);
+    System.out.println(stack);
+    System.out.println();
+    for(int i:stack)
+    {
+        System.out.print(i+" ");
+    }
+
+      System.out.println("\n"+stack.peek());
+      System.out.println("Item Popped "+stack.pop());
+    }
+}
