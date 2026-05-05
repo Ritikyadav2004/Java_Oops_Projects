4cb10e607bea639744ecac2a38a3753cf5098112
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-05-06 00:05:33 +0530

    RPolish Notation

---
+++ Stack/ReversePolishNotation.java
@@ -0,0 +1,50 @@
+package Stack;
+import java.util.*;
+public class ReversePolishNotation {
+    
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack= new Stack<>();
+        for(int i=0;i<tokens.length;i++)
+        {  
+
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) 
+           {
+              if(tokens[i].equals("+"))
+            {
+               int result=stack.pop()+stack.pop();
+               stack.push(result);
+            }
+            else if(tokens[i].equals("-"))
+            {    
+                int num1=stack.pop();
+                int num2=stack.pop();
+                 int result=num2-num1;
+                 stack.push(result);
+            }
+            else if(tokens[i].equals("*"))
+            {
+                 int result=stack.pop()*stack.pop();
+                stack.push(result);
+            }
+            else if(tokens[i].equals("/"))
+            {
+                  int num1=stack.pop();
+                int num2=stack.pop();
+                 int result=num2/num1;
+                
+                 stack.push(result);
+            }
+                
+           }
+           else 
+           {
+            // mean symbole 
+            stack.push(Integer.parseInt(tokens[i]));
+           }
+        }
+
+       return  stack.peek();
+    }
+
+}
