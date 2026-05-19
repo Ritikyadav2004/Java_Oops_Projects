114676ecf6364283f1aa996ce280d874d9097d8d
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-05-19 23:00:20 +0530

    Revise

--- Sliding_window/longestSubarrayWithSumlessThenTarget.java
+++ Sliding_window/longestSubarrayWithSumlessThenTarget.java
@@ -2,6 +2,7 @@ package Sliding_window;
 // concept of variable size window
 public class longestSubarrayWithSumlessThenTarget
 {
+    // revising
  public static void main(String[] args) {
     int arr[] = {1,2,3,2,4,5,6,5,6};
     int target = 17;
