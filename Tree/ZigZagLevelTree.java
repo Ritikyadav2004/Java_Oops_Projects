7cf1c4694ecfd090bf03a44f0e80888c4bce74a4
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-06-17 00:34:31 +0530

    Learning Tre

---
+++ Tree/ZigZagLevelTree.java
@@ -0,0 +1,61 @@
+package Tree;
+import java.util.*;
+public class ZigZagLevelTree {
+    
+
+
+//   Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+       
+        List<List<Integer>> ans= new ArrayList<>();
+ if(root==null) return ans;
+        Queue<TreeNode> q= new LinkedList<>();
+
+        boolean leftToRight=true;
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+                int size=q.size();
+                List<Integer> level=new ArrayList<>();
+                for(int i=0;i<size;i++)
+                {
+                    TreeNode node=q.poll();
+                    level.add(node.val);
+
+                    if(node.left!=null) q.offer(node.left);
+
+                    if(node.right!=null) q.offer(node.right);
+                }
+
+                if(!leftToRight)
+                {
+                    Collections.reverse(level);
+                }
+
+                leftToRight=!leftToRight;
+
+                ans.add(level);
+
+        }
+
+
+        return ans;
+
+
+
+    }
+}
