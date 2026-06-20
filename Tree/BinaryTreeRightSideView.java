9031588df703e8f13239e78275eac656fc288862
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-06-21 00:40:33 +0530

    TreeDaily

---
+++ Tree/BinaryTreeRightSideView.java
@@ -0,0 +1,55 @@
+package Tree;
+import java.util.*;
+public class BinaryTreeRightSideView {
+    
+
+
+// Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans= new ArrayList<>();
+        if(root==null)return ans;
+        Queue<TreeNode> q= new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            // List<Integer> temp= new ArrayList<>();
+
+            for(int i=0;i<size;i++)
+            {
+                TreeNode t=q.poll();
+                // temp.add(t.val);
+
+                if(t.left!=null)
+                {
+                    q.offer(t.left);
+
+                }
+                if(t.right!=null)
+                {
+                    q.offer(t.right);
+                }
+
+              if(i==size-1)
+             { ans.add(t.val);}
+
+            }
+           
+        }
+        return ans;
+    }
+}
